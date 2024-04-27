7bcb6762ba7248d5b1ae7ba9f07ced12789646cb
Darric Heng <darricheng@gmail.com>
2024-04-27 10:12:34 +0800

    update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,51 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-# starship init fish | source
+
+# Env vars
+set -gx EDITOR nvim
+
+# aliases
+alias zshrc="nvim ~/.zshrc"
+alias nconf="nvim ~/.config/nvim"
+alias tconf="nvim ~/.config/tmux"
+alias fconf="nvim ~/.config/fish"
+alias n="nvim"
+alias g="git"
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gcv="git commit --verbose"
+alias gco="git checkout"
+alias gp="git push"
+alias t="tmux"
+alias tk="tmux list-keys"
+alias f="fzf"
+alias man="batman"
+alias cz="chezmoi"
+
+# Additions to $PATH
+# Postgres
+fish_add_path /opt/homebrew/opt/postgresql@16/bin
+# make go binaries available on path
+fish_add_path $(go env GOPATH)/bin
+# Make brew's binutils available to the system
+fish_add_path /opt/homebrew/opt/binutils/bin
+# Use the homebrew-installed ruby instead of the one installed in macos by default
+fish_add_path /opt/homebrew/opt/ruby/bin
+# add packages installed by mason in Neovim
+fish_add_path /Users/darricheng/.local/share/nvim/mason/bin
+# llvm
+fish_add_path /opt/homebrew/opt/llvm/bin
+# Not sure if I'll need the below two when using clang?
+# export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+# export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+
+# zoxide
+zoxide init fish | source
+
+# starship prompt
+starship init fish | source
+
+# Set up fzf key bindings
+fzf --fish | source
