b8535c66490c4b6b29e03ba5a36dbb96eb607b9d
Darric Heng <darricheng@gmail.com>
2024-04-28 14:11:39 +0800

    use cz default formatting

--- dot_config/nvim/after/plugin/editor.lua
+++ dot_config/nvim/after/plugin/editor.lua
@@ -1,90 +1,95 @@
 -- Set the default colorscheme for my editor
-vim.cmd.colorscheme 'catppuccin-mocha'
+vim.cmd.colorscheme("catppuccin-mocha")
 
-require('no-neck-pain').setup {
-  width = 110,
-  mappings = {
-    -- enables key mappings for no-neck-pain
-    enabled = true,
-    -- toggle the plugin
-    toggle = '<leader>np',
-    -- increase the width (+5) of the main window.
-    widthUp = '<leader>n+',
-    -- decrease the width (-5) of the main window.
-    widthDown = '<leader>n-',
-    -- toggle the scratchpad feature.
-    scratchPad = '<leader>ns',
-  },
-}
+require("no-neck-pain").setup({
+	width = 110,
+	mappings = {
+		-- enables key mappings for no-neck-pain
+		enabled = true,
+		-- toggle the plugin
+		toggle = "<leader>np",
+		-- increase the width (+5) of the main window.
+		widthUp = "<leader>n+",
+		-- decrease the width (-5) of the main window.
+		widthDown = "<leader>n-",
+		-- toggle the scratchpad feature.
+		scratchPad = "<leader>ns",
+	},
+})
 -- NoNeckPain can be a bit buggy when more than one window is open,
 -- so this keymap resets the state of the windows by toggling the plugin twice
-vim.keymap.set('n', '<leader>nr', '<cmd>NoNeckPain<cr><cmd>NoNeckPain<cr>', { desc = '[N]oNeckPain: Reset (double toggle)' })
+vim.keymap.set(
+	"n",
+	"<leader>nr",
+	"<cmd>NoNeckPain<cr><cmd>NoNeckPain<cr>",
+	{ desc = "[N]oNeckPain: Reset (double toggle)" }
+)
 
 -- blankline indentation configs
-require('ibl').setup {
-  -- Setup config for indent_blankline goes here
-  -- See :h ibl.setup for more info
-  scope = {
-    show_start = false,
-    show_end = false,
-  },
-}
+require("ibl").setup({
+	-- Setup config for indent_blankline goes here
+	-- See :h ibl.setup for more info
+	scope = {
+		show_start = false,
+		show_end = false,
+	},
+})
 
 -- open undotree on the right and diff below
 vim.g.undotree_WindowLayout = 2
 -- width of the undotree window
 vim.g.undotree_SplitWidth = 40
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndoTree' })
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "[U]ndoTree" })
 
-vim.keymap.set('n', '<leader>z', '<cmd>ZenMode<cr>', { desc = '[Z]enMode' })
-require('zen-mode').setup {
-  plugins = {
-    tmux = {
-      enabled = true,
-    },
-  },
-}
+vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>", { desc = "[Z]enMode" })
+require("zen-mode").setup({
+	plugins = {
+		tmux = {
+			enabled = true,
+		},
+	},
+})
 
-require('notify').setup {
-  -- use non-opacity changing effect so that screen doesn't flicker
-  stages = 'slide',
-  max_width = 80,
-  max_height = 24,
-  fps = 60,
-}
+require("notify").setup({
+	-- use non-opacity changing effect so that screen doesn't flicker
+	stages = "slide",
+	max_width = 80,
+	max_height = 24,
+	fps = 60,
+})
 -- open the message history in telescope
-vim.keymap.set('n', '<leader>ms', function()
-  require('telescope').extensions.notify.notify()
-end, { desc = '[M]essage [S]earch' })
+vim.keymap.set("n", "<leader>ms", function()
+	require("telescope").extensions.notify.notify()
+end, { desc = "[M]essage [S]earch" })
 
-require('noice').setup {
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-      ['vim.lsp.util.stylize_markdown'] = true,
-      ['cmp.entry.get_documentation'] = true,
-    },
-  },
-  presets = {
-    bottom_search = false, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = true, -- add a border to hover docs and signature help
-  },
-  views = {
-    cmdline_popup = {
-      position = {
-        row = 20,
-        col = '50%',
-      },
-    },
-    cmdline_popupmenu = {
-      position = {
-        row = 23,
-        col = '50%',
-      },
-    },
-  },
-}
+require("noice").setup({
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true,
+		},
+	},
+	presets = {
+		bottom_search = false, -- use a classic bottom cmdline for search
+		command_palette = true, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = true, -- add a border to hover docs and signature help
+	},
+	views = {
+		cmdline_popup = {
+			position = {
+				row = 20,
+				col = "50%",
+			},
+		},
+		cmdline_popupmenu = {
+			position = {
+				row = 23,
+				col = "50%",
+			},
+		},
+	},
+})
