80b2099aa312cbf07c5781117d686e9335b97959
Darric Heng <darricheng@gmail.com>
2024-11-03 16:42:39 +0800

    add more elixir file formats and reorg formatter fts

--- dot_config/nvim/after/plugin/formatter.lua
+++ dot_config/nvim/after/plugin/formatter.lua
@@ -5,23 +5,34 @@ require("conform").setup({
 		lua = { "stylua" },
 		c = { "clang_format" },
 		cpp = { "clang_format" },
+		rust = { "rustfmt" },
+		go = { "goimports" },
+		python = { "black" },
+		toml = { "taplo" },
+		php = { "php_cs_fixer" },
+		json = prettier,
+		markdown = prettier,
+
+		-- Web
+		html = prettier,
+		css = prettier,
+
+		-- JS/TS
 		javascript = prettier,
 		typescript = prettier,
 		typescriptreact = prettier,
 		javascriptreact = prettier,
 		svelte = prettier,
-		html = prettier,
-		css = prettier,
-		json = prettier,
-		markdown = prettier,
-		rust = { "rustfmt" },
-		go = { "goimports" },
-		python = { "black" },
-		handlebars = { "djlint" },
-		toml = { "taplo" },
-		php = { "php_cs_fixer" },
+
+		-- templating languages
 		htmldjango = { "djlint" },
+		handlebars = { "djlint" },
+
+		-- Elixir
 		elixir = { "mix" },
+		eelixir = { "mix" },
+		heex = { "mix" },
+		surface = { "mix" },
 	},
 	format_on_save = {
 		lsp_format = "fallback",
