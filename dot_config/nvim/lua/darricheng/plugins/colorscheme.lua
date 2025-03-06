cbe6f3b9a3cc5d6a843adc28517248563efcfac7
Darric Heng <darricheng@gmail.com>
2025-03-06 20:25:37 +0800

    remove unnecessary colourschemes

--- dot_config/nvim/lua/darricheng/plugins/colorscheme.lua
+++ dot_config/nvim/lua/darricheng/plugins/colorscheme.lua
@@ -1,10 +1,6 @@
 return {
 	{ "catppuccin/nvim", name = "catppuccin", event = "VeryLazy" },
-	{ "navarasu/onedark.nvim", event = "VeryLazy" },
 	{ "folke/tokyonight.nvim", event = "VeryLazy" },
-	{ "rebelot/kanagawa.nvim", event = "VeryLazy" },
-	{ "EdenEast/nightfox.nvim", event = "VeryLazy" },
-	{ "sainnhe/everforest", event = "VeryLazy" },
 	{ "junegunn/seoul256.vim", event = "VeryLazy" },
 	{ "loctvl842/monokai-pro.nvim", lazy = false, opts = {
 		background_clear = { "float_win", "telescope" },
