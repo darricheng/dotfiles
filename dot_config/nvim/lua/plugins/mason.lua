b811f21054cbe9cd6dbe1c0a2dfe3435acb85df4
Darric Heng <darricheng@gmail.com>
2025-05-17 16:18:07 +0800

    remove mason tool installer Upon some thought, I don't need to ensure certain tools are installed. If I need them, I can easily install them by opening :Mason anyway.

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -7,30 +7,4 @@ return {
 	},
 	-- LSP
 	{ "williamboman/mason-lspconfig.nvim" },
-	-- Tool install helper
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		opts = {
-			-- a list of all tools you want to ensure are installed upon
-			-- start; they should be the names Mason uses for each tool
-			-- NOTE: LSPs are handled separately in lsp.lua, DAPs in dap.lua
-			-- TODO: Consolidate all the tool installation into this file?
-			ensure_installed = {
-				"clang-format",
-				-- javascript / typescript
-				"prettierd",
-				-- lua
-				"stylua",
-				"luacheck",
-				-- go
-				"goimports",
-				-- bash
-				"shellcheck", -- linter
-				"shfmt", -- formatter
-				-- python
-				"black",
-			},
-			auto_update = true,
-		},
-	},
 }
