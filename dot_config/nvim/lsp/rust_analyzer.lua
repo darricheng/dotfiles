fc8d0201f59a4bf6057e1ab89d60e1fc8954cfcd
Darric Heng <darricheng@gmail.com>
2025-05-18 16:06:33 +0800

    add rust_analyzer settings for leptos the settings should be merged with nvim-lspconfig's defaults see: https://neovim.io/doc/user/lsp.html#lsp-config

---
+++ dot_config/nvim/lsp/rust_analyzer.lua
@@ -0,0 +1,17 @@
+return {
+	-- Leptos DX settings
+	-- settings = {
+	-- 	["rust-analyzer"] = {
+	-- 		procMacro = {
+	-- 			ignored = {
+	-- 				leptos_macro = {
+	-- 					-- optional: --
+	-- 					-- "component",
+	-- 					"server",
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 		cargo = { features = { "ssr" } },
+	-- 	},
+	-- },
+}
