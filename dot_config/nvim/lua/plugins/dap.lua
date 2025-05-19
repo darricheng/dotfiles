015768e102f3a98de4f9b86c88320d78a2c90fa7
Darric Heng <darricheng@gmail.com>
2025-05-19 21:22:46 +0800

    remove unused plugin

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -162,28 +162,4 @@ return {
 			end, "Toggle [U]I")
 		end,
 	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- Makes a best effort to setup the various debuggers with
-			-- reasonable debug configurations
-			automatic_setup = true,
-
-			-- You can provide additional configuration to the handlers,
-			-- see mason-nvim-dap README for more information
-			-- NOTE: I think nil here is important so that I don't work with the defaults from mason-nvim-dap, which would make my debugging process harder
-			handlers = nil,
-
-			-- You'll need to check that you have the required things installed
-			-- online, please don't ask me how to install them :)
-			ensure_installed = {
-				-- Update this to ensure that you have the debuggers for the langs you want
-				"delve",
-				"js",
-				"codelldb",
-			},
-			automatic_installation = true,
-		},
-	},
 }
