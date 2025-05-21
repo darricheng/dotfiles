7bc7679cbd7357afd207c444a493b0fc149c6cbe
Darric Heng <darricheng@gmail.com>
2025-05-21 21:27:39 +0800

    init utils file with unused util fn

---
+++ dot_config/nvim/lua/utils.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+---Joins an
+---@param array string[]
+---@param separator string
+---@return string
+function M.join_strings(array, separator)
+	separator = separator or "," -- default separator is comma if not provided
+	local result = ""
+
+	for i, value in ipairs(array) do
+		if i > 1 then
+			result = result .. separator
+		end
+		result = result .. value
+	end
+
+	return result
+end
+
+return M
