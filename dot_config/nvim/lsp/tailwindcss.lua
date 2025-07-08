30d0c0dafb42d6686bc30b93dcadb93c79610ac7
Darric Heng <darric.heng@ailytics.ai>
2025-07-08 14:34:53 +0800

    chore: update comments with why

--- dot_config/nvim/lsp/tailwindcss.lua
+++ dot_config/nvim/lsp/tailwindcss.lua
@@ -4,6 +4,7 @@ return {
 			-- For use with the `class-variance-authority` library,
 			-- makes it such that we get tailwindCSS autocomplete in
 			-- the `cva` and `cx` functions.
+			-- Mainly needed because shadcn React uses this library.
 			-- See: https://cva.style/docs/getting-started/installation#intellisense
 			classFunctions = { "cva", "cx" },
 		},
