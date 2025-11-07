a29cbe04fd238697df48b650134c3c95fb44804a
Darric Heng <darric.heng@ailytics.ai>
2025-11-07 14:30:26 +0800

    update quicker keybindings

--- dot_config/nvim/lua/plugins/quickfix.lua
+++ dot_config/nvim/lua/plugins/quickfix.lua
@@ -22,17 +22,21 @@ return {
 				},
 			},
 		},
-		config = function()
-			vim.keymap.set("n", "<leader>q", function()
-				require("quicker").toggle()
-			end, {
+		keys = {
+			{
+				"<leader>q",
+				function()
+					require("quicker").toggle()
+				end,
 				desc = "Toggle quickfix",
-			})
-			vim.keymap.set("n", "<leader>l", function()
-				require("quicker").toggle({ loclist = true })
-			end, {
+			},
+			{
+				"<leader>l",
+				function()
+					require("quicker").toggle({ loclist = true })
+				end,
 				desc = "Toggle loclist",
-			})
-		end,
+			},
+		},
 	},
 }
