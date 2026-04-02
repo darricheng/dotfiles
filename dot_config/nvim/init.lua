405585d9ae572437b77cc2339349e96f67767f29
Darric Heng <darricheng@gmail.com>
2026-04-02 21:38:55 +0800

    migrate colourscheme to vim.pack

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,9 @@
 require("config")
 require("config.remap")
 require("config.set")
+
+vim.pack.add({
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+})
+
+vim.cmd.colorscheme("catppuccin-macchiato")
