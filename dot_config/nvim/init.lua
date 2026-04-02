94530c935fb757bb6f4747e76293b88804a966ac
Darric Heng <darricheng@gmail.com>
2026-04-02 21:52:59 +0800

    migrate nnp

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,6 +4,39 @@ require("config.set")
 
 vim.pack.add({
 	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	{ src = "https://github.com/shortcuts/no-neck-pain.nvim", version = vim.version.range("*") },
 })
 
 vim.cmd.colorscheme("catppuccin-macchiato")
+
+require("no-neck-pain").setup({
+	width = 100,
+	fallbackOnBufferDelete = false,
+	autocmds = {
+		enableOnVimEnter = false,
+	},
+	mappings = {
+		-- enables key mappings for no-neck-pain
+		enabled = true,
+		-- toggle the plugin
+		toggle = "<leader>np",
+		-- increase the width (+5) of the main window.
+		widthUp = "<leader>n+",
+		-- decrease the width (-5) of the main window.
+		widthDown = "<leader>n-",
+		-- toggle the scratchpad feature.
+		scratchPad = "<leader>ns",
+		debug = "<Leader>nD",
+	},
+})
+-- NoNeckPain can be a bit buggy when more than one window is open,
+-- so this keymap resets the state of the windows by toggling the plugin twice
+vim.keymap.set("n", "<leader>nr", function()
+	vim.cmd.NoNeckPain()
+	-- calling the function twice b2b didn't work after upgrading nvim to 0.10
+	-- would only be equal to calling the fn once
+	-- calling the second instance separately in a defer_fn seems to work fine
+	vim.defer_fn(function()
+		vim.cmd.NoNeckPain()
+	end, 100)
+end, { desc = "[N]oNeckPain: Reset (double toggle)" })
--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,47 +1,5 @@
 return {
 	{
-		-- Center the editor so that I don't have to constantly be looking to the left of my screen
-		"shortcuts/no-neck-pain.nvim",
-		version = "*",
-		event = "VeryLazy",
-		opts = {
-			width = 100,
-			fallbackOnBufferDelete = false,
-			autocmds = {
-				enableOnVimEnter = false,
-			},
-			mappings = {
-				-- enables key mappings for no-neck-pain
-				enabled = true,
-				-- toggle the plugin
-				toggle = "<leader>np",
-				-- increase the width (+5) of the main window.
-				widthUp = "<leader>n+",
-				-- decrease the width (-5) of the main window.
-				widthDown = "<leader>n-",
-				-- toggle the scratchpad feature.
-				scratchPad = "<leader>ns",
-				debug = "<Leader>nD",
-			},
-		},
-		keys = {
-			{ -- NoNeckPain can be a bit buggy when more than one window is open,
-				-- so this keymap resets the state of the windows by toggling the plugin twice
-				"<leader>nr",
-				function()
-					vim.cmd.NoNeckPain()
-					-- calling the function twice b2b didn't work after upgrading nvim to 0.10
-					-- would only be equal to calling the fn once
-					-- calling the second instance separately in a defer_fn seems to work fine
-					vim.defer_fn(function()
-						vim.cmd.NoNeckPain()
-					end, 100)
-				end,
-				desc = "[N]oNeckPain: Reset (double toggle)",
-			},
-		},
-	},
-	{
 		"folke/zen-mode.nvim",
 		cmd = "ZenMode",
 		keys = {
