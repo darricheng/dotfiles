6af9fc2a24ca9e471519fdb05278847dbfae727e
Darric Heng <darricheng@gmail.com>
2026-04-03 09:55:11 +0800

    fix: and the type annotation

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,7 +5,7 @@ require("config.set")
 ---@param modes string|string[] Mode "short-name" (see |nvim_set_keymap()|), or a list thereof.
 ---@param lhs string           Left-hand side |{lhs}| of the mapping.
 ---@param rhs string|function  Right-hand side |{rhs}| of the mapping, can be a Lua function.
----@param opts? map.Opts
+---@param opts? vim.keymap.set.Opts
 local map = function(modes, lhs, rhs, opts)
 	vim.keymap.set(modes, lhs, rhs, opts)
 end
