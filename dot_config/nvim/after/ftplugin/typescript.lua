eaa4fc0c1a4b3dd093c2f99703086dd534709611
Darric Heng <darricheng@gmail.com>
2026-05-06 22:05:27 +0800

    remove old nvim-treesitter configs

--- dot_config/nvim/after/ftplugin/typescript.lua
+++
@@ -1,2 +0,0 @@
-require("nvim-treesitter").install({ "jsdoc", "regex", "typescript" })
-vim.treesitter.start()
--- dot_config/nvim/after/ftplugin/typescriptreact.lua
+++
@@ -1,2 +0,0 @@
-require("nvim-treesitter").install({ "jsdoc", "regex", "tsx" })
-vim.treesitter.start()
