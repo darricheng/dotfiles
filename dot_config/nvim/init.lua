ef710d319b9c0aadfe8cfb3cb56079ce9c4e619c
Darric Heng <darricheng@gmail.com>
2026-06-02 11:37:40 +0800

    formatting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -513,16 +513,16 @@ require("arborist").setup({
 -- nvim-treesitter-textobjects
 require("nvim-treesitter-textobjects").setup()
 vim.keymap.set({ "x", "o" }, "af", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+	require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
 end)
 vim.keymap.set({ "x", "o" }, "if", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+	require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
 end)
 vim.keymap.set({ "x", "o" }, "ac", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+	require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
 end)
 vim.keymap.set({ "x", "o" }, "ic", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+	require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
 end)
 
 require("treesitter-context").setup({
